5fcf0c7898e6aca1556a443d65adb7f8ddab8f0e
Cam Burhans <cburhans@email.unc.edu>
2023-01-26 19:30:16 -0500

    Trying again - pt.5

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const args = minimist(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 const port = args.port || 3000;
+const hostname = os.hostname();
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
@@ -40,7 +41,7 @@ const server = http.createServer((req, res) => {
       res.end(filedata);
     });
   
-  server.listen(port, () => console.log(`Server listening on port ${port}`));
+  server.listen(port, hostname, () => console.log(`Server listening on port ${port}`));
 
 
 
